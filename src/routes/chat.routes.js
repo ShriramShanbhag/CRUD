0a46bb7773a39d6a9867a881193624b985f06509
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-01-16 17:15:19 +0530

    Updated review comments

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { requireAuth } from '../middlewares/requireAuth.js';
 import { requirePermission } from '../middlewares/authorize.js';
 import { createChat, deleteChat, getChatById, getMyChats } from '../controllers/chat.conroller.js';
+import { PERMISSIONS } from '../config/permissions.js';
 
 const router = express.Router();
 
-router.post('', requireAuth, requirePermission, createChat);
-router.get('/:id', requireAuth, requirePermission, getChatById);
-router.get('', requireAuth, requirePermission, getMyChats);
-router.delete('', requireAuth, requirePermission, deleteChat);
+router.post('', requireAuth, requirePermission(PERMISSIONS.CHAT_CREATE), createChat);
+router.get('/:id', requireAuth, requirePermission(PERMISSIONS.CHAT_READ), getChatById);
+router.get('', requireAuth, requirePermission(PERMISSIONS.CHAT_READALL), getMyChats);
+router.delete('/:id', requireAuth, requirePermission(PERMISSIONS.CHAT_DELETE), deleteChat);
 
 export default router;
--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -1,19 +1,20 @@
+import { PERMISSIONS } from "../config/permissions.js";
 import AppError from "../errors/AppError.js";
 import chatRepo from "../repositories/chat.repository.js";
 import { assertResourceAccess } from "../utils/authorization.js";
 
 const ChatService = { 
     getChatById: async ({actor, resourceId}) => {
-        assertResourceAccess(actor, resourceId)
         const chat =  await chatRepo.findById(resourceId);
         if(!chat) throw new AppError("Not found", 404)
+        assertResourceAccess(actor, chat.user_id, PERMISSIONS.CHAT_READ);
         return chat;
     },
     getMyChats: async ({actor}) => {
         return await chatRepo.findAllByUserId(actor.id);
     },
     getAllChatsByUser: async ({actor, user_id}) => {
-        assertResourceAccess(actor, user_id);
+        assertResourceAccess(actor, user_id, PERMISSIONS.CHAT_READALL);
         return await chatRepo.findAllByUserId(user_id);
     },
     createChat: async ({actor, body}) => {
@@ -26,9 +27,9 @@ const ChatService = {
         })
     },
     deleteChat: async ({actor, resourceId}) => {
-        assertResourceAccess(actor, resourceId);
         const chat = await chatRepo.findById(resourceId);
         if(!chat) throw new AppError("Chat does not exist", 404);
+        assertResourceAccess(actor, chat.user_id, PERMISSIONS.CHAT_DELETE);
         return chatRepo.delete(resourceId);
     },
 }
