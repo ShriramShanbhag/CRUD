a3695dc502cb43c38c28bcb340860907ef54b0e9
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-01-16 18:10:27 +0530

    Updated review comments on migration

--- db/migrations/20260116122035_create_tags_tables.js
+++ db/migrations/20260116122035_create_tags_tables.js
@@ -4,16 +4,18 @@
  */
 exports.up = async function(knex) {
   await knex.schema.createTable('tags', (table) => {
-    table.increments('id').notNullable();
+    table.increments('id').primary();
     table.string('name', 100).unique().notNullable();
   })
 
   await knex.schema.createTable('chat_tags', (table) => {
-    table.integer('chat_id').notNullable();
-    table.integer('tag_id').notNullable();
+    table.integer('chat_id').unsigned().notNullable();
+    table.integer('tag_id').unsigned().notNullable();
 
-    table.foreign('chat_id').references('id').inTable('chats').onDelete('CASCADE')
-    table.foreign('tag_id').references('id').inTable('tags').onDelete('CASCADE')
+    table.foreign('chat_id').references('id').inTable('chats').onDelete('CASCADE');
+    table.foreign('tag_id').references('id').inTable('tags').onDelete('CASCADE');
+
+    table.primary(['chat_id', 'tag_id']);
   })
 };
 
@@ -22,6 +24,6 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
-    await knex.schema.dropTable('tags');
     await knex.schema.dropTable('chat_tags');
+    await knex.schema.dropTable('tags');
 };
