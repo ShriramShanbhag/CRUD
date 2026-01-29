2a46df9abbb52118f0ec774c247912c41b619219
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-01-29 19:08:53 +0530

    updated schema for chat

--- src/validations/chat.schema.js
+++ src/validations/chat.schema.js
@@ -3,7 +3,7 @@ import z from 'zod';
 const PLATFORMS = ["chatgpt", "gemini", "perplexity"]
 
 export const createChatSchema = z.object({
-    title: z.string().length(100).nonempty().trim(),
+    title: z.string().max(100).nonempty().trim(),
     url: z.url().nonempty().trim(),
     user_id: z.int().nonnegative(),
     platform: z.string().enum(PLATFORMS)
@@ -11,7 +11,7 @@ export const createChatSchema = z.object({
 
 export const addTagToChatSchema = z.object({
     tagName: z.string().optional(),
-    tagId: z.string(20).optional()
+    tagId: z.int().optional()
 }).refine((data) => data.tagName || data.tagId, {
     message: "Either tagname or tag id should be present"
 })
